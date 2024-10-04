65c13b456c9bbb87ca67009597e6e6a3a11123cc
saied <saied.nasiripour@gmail.com>
2024-10-04 16:07:48 +0330

    Delete .history directory

--- .history/src/main_20241003154700.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-function App() {
-  return <h1>Hello React!</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
