500de5b5a35ec51f7e633cd6cb96d3785cb6e15f
saied-nasiripour <saied.nasiripour@gmail.com>
2024-10-06 17:11:54 +0330

    add pure header, menu and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import React from "react";
+
+function Pizza() {
+  return (
+    <>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </>
+  );
+}
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()}. We&apos;re currently open!
+    </footer>
+  );
+  // return React.createElement("footer", null, "We&apos;re currently open!");
 }
 
 export default App;
