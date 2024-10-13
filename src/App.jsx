37f033cb3b127b7b935131128c37aacf8e71a6eb
saied-nasiripour <saied.nasiripour@gmail.com>
2024-10-13 16:37:49 +0330

    add some style to menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import pizzaData from '../public/data.js';
+
 function App() {
   return (
     <div className="container">
@@ -19,41 +21,65 @@ function Header() {
 }
 
 function Menu() {
-  return (
-    <main className="menu">
-      <h2>Our Menu</h2>
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-    </main>
-  );
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+            <ul className="pizzas">
+                {
+                    pizzaData.map(
+                        (pizza, idx) => (
+                            <Pizza
+                                key={idx}
+                                pizzaObj={pizza}
+                            />
+                        )
+                    )
+                }
+            </ul>
+{/*            <ul className="pizzas">
+                {
+                    pizzaData.map(
+                        (pizza, idx) => {
+                            return <Pizza
+                                key={idx}
+                                pizzaObj={pizza}
+                            />
+                        }
+                    )
+                }
+            </ul>*/}
+            {/*<Pizza
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={10}
+            />*/}
+        </main>
+    );
 }
 
 function Pizza(props) {
-  return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
-      <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
-      </div>
-    </div>
-  );
+    return (
+        <li className="pizza">
+            <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}/>
+            <div>
+                <h3>{props.pizzaObj.name}</h3>
+                <p>{props.pizzaObj.ingredients}</p>
+                <span>{props.pizzaObj.price + 3}</span>
+            </div>
+        </li>
+    );
 }
 
 function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-  console.debug("isOpen : ", isOpen);
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.debug("isOpen : ", isOpen);
 
-  return (
-    <footer className="footer">
+    return (
+        <footer className="footer">
       {new Date().toLocaleTimeString()}. We&apos;re currently open!
     </footer>
   );
